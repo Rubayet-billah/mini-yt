e471b331c8d29f0fa1e63baa5c76afeddac62b3b
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-07 02:59:45 +0600

    folder stucture complete

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
+import Tags from "../../components/Tags/Tags";
+import Pagination from "../../components/UI/Pagination";
+import VideoGrid from "../../components/VideoGrid/VideoGrid";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Tags />
+      <VideoGrid />
+      <Pagination />
+      <Footer />
     </div>
   );
 };
