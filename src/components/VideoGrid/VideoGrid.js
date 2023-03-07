b89ee7e95f87341b86342fa79e47ddf03024d694
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-07 17:37:26 +0600

    ui updated

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideosThunk } from "../../features/videos/videosSlice";
+import Loading from "../UI/Loading";
 import VideoGridItem from "./VideoGridItem";
 
 const VideoGrid = () => {
+  const { videos, isLoading, isError, error } = useSelector(
+    (state) => state.videos
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchVideosThunk());
+  }, [dispatch]);
+
+  let content = "";
+  if (isLoading) {
+    content = <Loading />;
+  }
+  if (!isLoading && isError) {
+    content = <div className="col-span-12">{error}</div>;
+  }
+  if (!isLoading && !isError && videos.length === 0) {
+    content = <div className="col-span-12">No videos found</div>;
+  }
+  if (!isLoading && !isError && videos.length > 0) {
+    content = videos?.map((video) => <VideoGridItem video={video} />);
+  }
+
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-          <VideoGridItem />
+          {content}
         </div>
       </section>
     </section>
--- src/components/VideoGrid/VideoGridItem.js
+++ src/components/VideoGrid/VideoGridItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const VideoGridItem = () => {
+const VideoGridItem = ({ video }) => {
+  const {} = video;
   return (
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
