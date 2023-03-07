1ff9ef9a163ee2abe8d81aca2764a20176d67973
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-07 22:27:03 +0600

    fetched right url

--- src/features/relatedVideo/relatedVideosApi.js
+++ src/features/relatedVideo/relatedVideosApi.js
@@ -1,7 +1,7 @@
 import axiosInstance from "../../utils/axios";
 
 const getQueryString = (id, tags) =>
-  tags?.map((tag) => `tags_like=${tag}`).join("&") + `&ne=${id}&limit=${5}`;
+  tags?.map((tag) => `tags_like=${tag}`).join("&") + `&id_ne=${id}&limit=${5}`;
 
 export const fetchRelatedVideos = async ({ id, tags }) => {
   const res = await axiosInstance.get(`/videos?${getQueryString(id, tags)}`);
